a9bfcb16480ef63744f68dc92cd5c2a5fc250837
bacca <bacca2095@gmail.com>
2022-07-08 11:05:18 -0500

    feat: add route test component

---
+++ src/app/components/test/test.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss'],
+})
+export class TestComponent implements OnInit {
+  routeWithParams!: boolean;
+  text!: string;
+
+  constructor(private readonly route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      const { test1, test2 } = params;
+      if (test1 && test2) {
+        this.routeWithParams = true;
+        this.executeActionIfRouteContainParams(params);
+      } else {
+        this.routeWithParams = false;
+      }
+    });
+  }
+
+  executeActionIfRouteContainParams(params: any) {
+    const objectKeys = Object.keys(params);
+    const paramsToText = objectKeys
+      .map((key) => `${key}=${params[key]}`)
+      .join(', ');
+    this.text = `This route contains the below values for query params ${paramsToText}`;
+  }
+}
