92dd12a7a08bb95cc3c0bb69ccb45a5b04594a20
bacca <bacca2095@gmail.com>
2022-07-08 11:51:57 -0500

    fix: button to navigate with query params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'rutas';
+  constructor(private readonly router: Router) {}
+
+  navigateWithQueryParams() {
+    this.router.navigate(['test'], {
+      queryParams: {
+        test1: 2000,
+        test2: 3000,
+      },
+    });
+  }
 }
